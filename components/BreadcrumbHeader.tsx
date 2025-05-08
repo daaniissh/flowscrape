f986edcc07c66dd4c43adffef8c97f578e00c8d9
Danish <102005928+daaniissh@users.noreply.github.com>
2025-05-09 00:54:32 +0530

    feat: mobile navigation (#2)
    * feat: not found page
    
    * style: added responive button
    
    * feat: custome class
    
    * feat: mobile navigation menu

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -2,12 +2,14 @@
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb'
+import { MobileSidebar } from './DesktopSidebar'
 
 const BreadcrumbHeader = () => {
   const pathName = usePathname()
   const paths = pathName === "/" ? [""] : pathName?.split("/")
   return (
     <div className='flex items-center flex-start' >
+      <MobileSidebar/>
       <Breadcrumb>
         <BreadcrumbList>
           {paths.map((path, index) => (
