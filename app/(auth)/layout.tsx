eef45fe53a3a1ac73c29836cfe7ba25db8d40993
daaniissh <pkdanish329@gmail.com>
2025-05-09 18:38:58 +0530

    feat(auth): add auth routes (sign-in, sign-up, etc)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import Logo from '@/components/Logo'
+import React from 'react'
+
+const layout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='flex-center flex-col h-screen gap-4' >
+      <Logo/>
+      {children}
+    </div>
+  )
+}
+
+export default layout
