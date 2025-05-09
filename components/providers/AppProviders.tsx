eac2ca165860e148def04da81f0a4f3bd7856d55
daaniissh <pkdanish329@gmail.com>
2025-05-09 18:38:39 +0530

    refactor(providers): extend AppProviders to include auth context

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,7 +1,10 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
+import { ClerkProviderWrapper } from "../ClerkThemeProvider"
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
+  return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ClerkProviderWrapper>{children}</ClerkProviderWrapper>
+  </ThemeProvider>
 }
