3fc134970796c2773739eb1139a181a35d719705
daaniissh <pkdanish329@gmail.com>
2025-05-12 23:32:51 +0530

    fix: sidebar navigation issue

--- components/DesktopSidebar.tsx
+++ components/DesktopSidebar.tsx
@@ -8,7 +8,7 @@ import { usePathname } from 'next/navigation'
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 const routes = [
   {
-    href: "",
+    href: "/",
     label: "Home",
     icon: HomeIcon,
   },
@@ -30,7 +30,9 @@ const routes = [
 ]
 const DesktopSidebar = () => {
   const pathname = usePathname()
-  const activeRoute = routes.find(route => route.href.length > 0 && pathname.includes(route.href)) || routes[0]
+  const activeRoute = routes.find(route =>
+    route.href === "" ? pathname === "/" : pathname === `/${route.href}`
+  ) || routes[0];
   return (
     <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'  >
       <div className="flex items-center justify-center gap-2 border-b-[1px] p-4">
@@ -51,12 +53,14 @@ const DesktopSidebar = () => {
 export function MobileSidebar() {
   const [isOpen, setOpen] = useState(false)
   const pathname = usePathname()
-  const activeRoute = routes.find(route => route.href.length > 0 && pathname.includes(route.href)) || routes[0]
+  const activeRoute = routes.find(route =>
+    route.href === "" ? pathname === "/" : pathname === `/${route.href}`
+  ) || routes[0];
   return <div className='block border-separate bg-background md:hidden' >
     <nav className="container flex-center">
       <Sheet open={isOpen} onOpenChange={setOpen}>
         <SheetTrigger>
-          <Button  onClick={() => setOpen(prev => !prev)} variant="ghost" size={"icon"}>
+          <Button onClick={() => setOpen(prev => !prev)} variant="ghost" size={"icon"}>
             <MenuIcon />
           </Button>
         </SheetTrigger>
@@ -64,7 +68,7 @@ export function MobileSidebar() {
           <Logo />
           <div className="flex flex-col gap-1">
             {routes.map(route => (
-              <Link key={route.href} href={route.href} className={buttonVariants({
+              <Link onClick={(prev)=>setOpen(!prev)} key={route.href} href={route.href} className={buttonVariants({
                 variant: activeRoute.href === route.href ? "sideBarActivateVariant" : "sideBarItem"
               })} > <route.icon size={20} />{route.label} </Link>
             ))}
