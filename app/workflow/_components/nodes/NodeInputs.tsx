28a46a056c10a751444b86d6595cb64ba8976429
Danish <102005928+daaniissh@users.noreply.github.com>
2025-05-13 15:21:34 +0530

    feat: workflow editor launch browser UI (#10)
    * feat: not found page
    
    * style: added responive button
    
    * feat: custome class
    
    * feat: mobile navigation menu
    
    * refactor(dashboard): update layout to support Clerk auth and theme
    
    * style(auth): add custom styles for Clerk components
    
    * feat(root): integrate ClerkProvider with theme support in root layout
    
    * refactor(providers): extend AppProviders to include auth context
    
    * chore(deps): add @clerk/nextjs and related auth dependencies
    
    * feat(auth): add auth routes (sign-in, sign-up, etc)
    
    * feat(theme): create dynamic Clerk theme provider component
    
    * feat(security): implement auth middleware for route protection
    
    * fix: clerk form theme
    
    * feat: workflow add and delete
    
    * feat: workflow add and delete
    
    * feat: prisma superbase intgrated
    
    * chore: ignore prisma generated files and local db
    
    * chore: resolved prisma merge conflicts and ignored generated files
    
    * chore: resolved prisma merge conflicts and ignored generated files
    
    * chore: gitignore
    
    * style: workflow skelton
    
    * chore: resolved prisma merge conflicts and ignored generated files
    
    * feat: workflow editor launch browser Ui

---
+++ app/workflow/_components/nodes/NodeInputs.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils'
+import { TaskParam } from '@/types/task'
+import { Handle, Position } from '@xyflow/react'
+import React, { ReactNode } from 'react'
+import NodeParamsField from './NodeParamsField'
+
+const NodeInputs = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className='flex flex-col divide-y gap-2' >{children}</div>
+  )
+}
+
+export default NodeInputs
+
+export function NodeInput({ input }: { input: TaskParam }) {
+  return (
+    <div className='flex justify-start bg-secondary relative p-3  w-full' >
+      {/* <input.icon size={16} /> */}
+      <NodeParamsField param={input}/>
+      {!input.hideHandle && (
+        <Handle id={input.name} type="target" position={Position.Left} className={cn("!bg-muted-foreground !border-2 !border-background !-left-2 !w-4 !h-4")} />
+      )}
+
+
+    </div>
+  )
+
+}
---
+++ lib/workflow/task/LaunchBrowser.tsx
@@ -0,0 +1,20 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { GlobeIcon, LucideProps } from "lucide-react";
+
+export const LaunchBrowserTask = {
+  type: TaskType.LAUNCH_BROWSER,
+  label: "Launch browser",
+  icon: (props: LucideProps) => (
+    <GlobeIcon className="stroke-pink-400 size-8" {...props} />
+  ),
+  isEntryPoint: true,
+  inputs: [
+    {
+      name: "website url",
+      type:TaskParamType.STRING,
+      helperText: "eg: https://www.google.com",
+      required: true,
+      hideHandle:true
+    }
+  ]
+};
